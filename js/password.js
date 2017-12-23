10e9e83431c7c829fdaff6a08e623bf77735ec6f
mixyznik <mixy80@gmail.com>
2017-12-23 15:43:47 +0100

    third commit

--- js/password.js
+++ js/password.js
@@ -3,24 +3,40 @@ var pw=document.getElementById("pass");
 function checkStrength() {
 	var pas=pw.value;
 
+var pro=0;
 if (pas.length < 8) {
 	//$("prvi").html("unesite dovoljan broj karaktera");
 document.getElementById("cetvrti").innerHTML="vise";
 }
 if (pas.length >= 8){
 	document.getElementById("cetvrti").innerHTML="tvoj Mojo je veci za +1";
+pro=pro+20;
+document.getElementById("myProgress").value =pro;
+
 }
 if (pas.match(/([0-9])/)) {
 	document.getElementById("drugi").innerHTML="tvoj Mojo je veci za +1";
+pro=pro+20;
+document.getElementById("myProgress").value =pro;
+
 }
 if (pas.match(/([a-z])/)) {
 	document.getElementById("treci").innerHTML="tvoj Mojo je veci za +1";
+	pro=pro+20;
+document.getElementById("myProgress").value =pro;
+
 }
  if (pas.match(/([A-Z])/)) {
 	document.getElementById("prvi").innerHTML="tvoj Mojo je veci za +1";
+	pro=pro+20;
+document.getElementById("myProgress").value =pro;
+
 }
 if (pas.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
 	document.getElementById("peti").innerHTML="tvoj Mojo je veci za +1";
+	pro=pro+20;
+document.getElementById("myProgress").value =pro;
+
 }
 if ((pas.length >= 8)&&(pas.match(/([0-9])/))&&(pas.match(/([a-z])/))&&(pas.match(/([A-Z])/))&&(pas.match(/([!,%,&,@,#,$,^,*,?,_,~])/))) {
 	myTable.setAttribute("bgcolor","#FF0000");
@@ -33,3 +49,4 @@ if ((pas.length >= 8)&&(pas.match(/([0-9])/))&&(pas.match(/([a-z])/))&&(pas.matc
 
 }
 //});
+
